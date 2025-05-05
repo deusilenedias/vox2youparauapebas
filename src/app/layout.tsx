c944b669f97111a291924fec4897b0ee0e699baf
maxinelobato <limabarreto.rg@gmail.com>
2025-05-05 13:13:44 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,21 +29,23 @@ export default function RootLayout({
         suppressHydrationWarning={true}
         className={`${montserrat.className} antialiased bg-brandGray50`}
       >
-        <main>{children}</main>
-        <noscript
-          dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
-          }}
-        />
-        <Script id="clarity-script" strategy="afterInteractive">
-          {`
+        <main>
+          {children}
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`
             (function(c,l,a,r,i,t,y){
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
             })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
           `}
-        </Script>
+          </Script>
+        </main>
       </body>
     </html>
   )
