2ec06d66ed6c87991cb9fe2afec6aeafd85a99c5
maxinelobato <limabarreto.rg@gmail.com>
2025-06-02 19:59:11 -0300

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,25 +27,12 @@ export default function RootLayout({
         })(window,document,'script','dataLayer','${GTM_ID}');
         `}
       </Script>
-
       <body
         suppressHydrationWarning={true}
         className={`${montserrat.className} antialiased bg-brandGray50`}
       >
         <main>
           {children}
-          <Script
-            src="//www.clarity.ms/tag/reuogveczj"
-            strategy="afterInteractive"
-          >
-            {`
-        (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "reuogveczj");
-        `}
-          </Script>
           <noscript
             dangerouslySetInnerHTML={{
               __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
